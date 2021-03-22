2d1a0a40ef938eede399f81cc791241463a0a086
rgirodon <remy.girodon@gmail.com>
2021-03-22 10:01:10 +0100

    Improve Thread Pool Executor

--- thread_pool_executor.py
+++ thread_pool_executor.py
@@ -1,22 +1,17 @@
 import concurrent.futures
-import logging
 import threading
 import time
 
 def thread_function(name):
 
-    logging.info("Thread %s: starting", name)
+    print("Thread " + str(name) + ": starting")
     
     time.sleep(2)
     
-    logging.info("Thread %s: finishing", name)
+    print("Thread " + str(name) + ": finishing")
 
 if __name__ == "__main__":
     
-    format = "%(asctime)s: %(message)s"
-    
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
 
-        executor.map(thread_function, range(3))
+        executor.map(thread_function, range(6))
